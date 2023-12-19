8804cb5f2fe96924f52bccd2c6a030cd61bafbe2
Thiago Alves <80608138+Thiagof2755@users.noreply.github.com>
2023-12-19 16:10:07 -0300

    inicio do primeiro componente

---
+++ src/Components/Logo/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../../Images/logo.png';
+
+function Logo  ()  {
+  return (
+    <div className='logo'>
+        
+      <img src={logo} 
+      alt="logo" 
+      className='logo-img '/>
+
+    </div>
+  );
+};
+
+export default MeuComponente;
